6cf8a73129c7f209baddf4f4bff90e3e4792476a
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-28 17:07:01 +0200

    One-to-One association User Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :rooms, dependent: :destroy
+  has_one :profile
+
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
+
+  def full_name
+    profile.full_name
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :rooms
+  resources :profiles, only: [:new, :edit, :create, :update]
 
   devise_for :users
 end
